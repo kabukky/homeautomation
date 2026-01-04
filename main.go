1d9b8a2e49740842aa8a07a5b6a2e034126de56b
Hagelstein, Kai <kai.hagelstein@spiegel.de>
2026-01-04 14:00:08 +0100

    Fix dryer

--- main.go
+++ main.go
@@ -14,23 +14,23 @@ func main() {
 		runtime.LockOSThread()
 		if utils.CameraDebugViaNetwork {
 			// Dryer
-			// image, err := camera.GetImageRtsp(utils.CameraHostDryer, utils.CameraUsername, utils.CameraPassword)
-			// if err != nil {
-			// 	log.Fatalln(err)
-			// }
-			// digits, err := camera.RecognizeDryer(*image)
-			// if err != nil {
-			// 	log.Fatalln(err)
-			// }
-			// Washing machine
-			imageBytes, err := camera.GetImage(utils.CameraHostWashingMachine)
+			image, err := camera.GetImageRtsp(utils.CameraHostDryer, utils.CameraUsername, utils.CameraPassword)
 			if err != nil {
 				log.Fatalln(err)
 			}
-			digits, err := camera.RecognizeWashingMachine(imageBytes)
+			digits, err := camera.RecognizeDryer(*image)
 			if err != nil {
 				log.Fatalln(err)
 			}
+			// // Washing machine
+			// imageBytes, err := camera.GetImage(utils.CameraHostWashingMachine)
+			// if err != nil {
+			// 	log.Fatalln(err)
+			// }
+			// digits, err := camera.RecognizeWashingMachine(imageBytes)
+			// if err != nil {
+			// 	log.Fatalln(err)
+			// }
 
 			log.Println(digits)
 		}
